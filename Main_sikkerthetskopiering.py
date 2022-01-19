dc5946c010f07b6f632b304198535b064ce82ef4
kevin333221 <kevinmathias@hotmail.no>
2022-01-19 11:07:16 +0100

    created 'creating_enemies'

--- Main_sikkerthetskopiering.py
+++ Main_sikkerthetskopiering.py
@@ -25,42 +25,48 @@ user = Player.player(screen_w, screen_h)
 ball1 = Ball.basic_ball(screen_w, screen_h, universal_speed)
 messuring_enemy = Enemies.basic_enemy(screen_w, screen_h, 0, 40, 100, enemy_color)
 
-# Initalizing Enemy values
-all_enemies_length = distance_between_other_enemies*num_of_enemies
+def creating_enemies(num_of_enemies):
+    counter = 0
+    all_enemies_length = distance_between_other_enemies*num_of_enemies
+    enemy_width = 40
 
-# Calculating the total width of x number of enemies on the screen
-if all_enemies_length > screen_w:
-    while messuring_enemy.pos.x + distance_between_other_enemies < screen_w:
-        messuring_enemy.pos.x += distance_between_other_enemies
-else:
-    while messuring_enemy.pos.x + distance_between_other_enemies < all_enemies_length:
-        messuring_enemy.pos.x += distance_between_other_enemies
-
-# Calculating the width of the enemies depending on the screen size
-if messuring_enemy.w >= distance_between_other_enemies:
-    messuring_enemy.w = distance_between_other_enemies - 10
-
-enemy_spawn_shift = (screen_w - messuring_enemy.pos.x)/2 + 5
-bots = []
-counter = 0
-enemy_ypos = 40
-
-# Creating enemies
-for x in range(num_of_enemies):
-    enemy_color = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
-    enemy_xpos = counter*distance_between_other_enemies
-    enemy = Enemies.basic_enemy(screen_w, screen_h, enemy_xpos + enemy_spawn_shift, enemy_ypos, messuring_enemy.w, enemy_color)
-    if enemy.pos.y + 50 < screen_h - 300:
-        if enemy.pos.x + enemy.w < screen_w:
-            counter += 1
-        else:
-            enemy.pos.x = enemy_spawn_shift
-            enemy.pos.y += 50
-            enemy_ypos += 50
-            counter = 1
-        bots.append(enemy)
+    # Calculating the total width of x number of enemies on the screen
+    if all_enemies_length > screen_w:
+        while messuring_enemy.pos.x + distance_between_other_enemies < screen_w:
+            messuring_enemy.pos.x += distance_between_other_enemies
     else:
-        num_of_enemies -= 1
+        while messuring_enemy.pos.x + distance_between_other_enemies < all_enemies_length:
+            messuring_enemy.pos.x += distance_between_other_enemies
+
+    # Calculating the width of the enemies depending on the screen size
+    if messuring_enemy.w >= distance_between_other_enemies:
+        messuring_enemy.w = distance_between_other_enemies - 10
+
+    enemy_spawn_shift = (screen_w - messuring_enemy.pos.x)/2 + 5
+    bots = []
+    enemy_ypos = 40
+
+    # Creating enemies
+    for x in range(num_of_enemies):
+        enemy_color = (random.randint(100, 255), random.randint(100, 255), random.randint(100, 255))
+        enemy_xpos = counter*distance_between_other_enemies
+        enemy = Enemies.basic_enemy(screen_w, screen_h, enemy_xpos + enemy_spawn_shift, enemy_ypos, messuring_enemy.w, enemy_color)
+        if enemy.pos.y + 50 < screen_h - 300:
+            if enemy.pos.x + enemy.w < screen_w:
+                counter += 1
+            else:
+                enemy.pos.x = enemy_spawn_shift
+                enemy.pos.y += 50
+                enemy_ypos += 50
+                counter = 1
+            bots.append(enemy)
+        else:
+            num_of_enemies -= 1
+
+    return bots
+    
+bots = creating_enemies(num_of_enemies)
+
 clock = pygame.time.Clock()
 
 running = True
